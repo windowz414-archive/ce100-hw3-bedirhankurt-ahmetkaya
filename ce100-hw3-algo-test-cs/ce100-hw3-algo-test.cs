7a437aedb3b60df05d0166cc1e26c1c455f0b8b7
Ahmet KAYA <ahmet.kaya.ceng@gmail.com>
2023-05-13 13:42:13 +0300

    commit

---
+++ ce100-hw3-algo-test-cs/ce100-hw3-algo-test.cs
@@ -0,0 +1,93 @@
+using ce100_hw3_algo_lib;
+using System.IO;
+using System.Text;
+using Xunit;
+
+public class HuffmanTreeTests
+{
+    [Fact]
+    public void CompressAndDecompressTest()
+    {
+        // Arrange
+        string musicFilePath = @"../../../grind.mp3";
+        string randomFilePath = @"random.txt";
+
+        // Generate a random text file filled with Lorem Ipsum text until the music and text files are of the same size
+        int musicFileSize = (int)new FileInfo(musicFilePath).Length;
+        string randomText = LoremIpsumGenerator.GenerateLoremIpsum(musicFileSize);
+        File.WriteAllText(randomFilePath, randomText);
+
+        // Compress the music and random files using Huffman coding
+        string compressedMusicFilePath = @"compressedgrind.mp3";
+        string compressedRandomFilePath = @"compressedrandom.txt";
+        byte[] musicinput = File.ReadAllBytes(musicFilePath);
+        byte[] randominput = File.ReadAllBytes(randomFilePath);
+        byte[] musicoutput = HuffmanTree.Compress(musicinput);
+        HuffmanTree.Compress(randomFilePath, compressedRandomFilePath);
+
+        // Decompress the compressed music and random files
+        string decompressedMusicFilePath = @"path/to/decompressed/music/file";
+        string decompressedRandomFilePath = @"path/to/decompressed/random/file";
+        HuffmanTree.Decompress(compressedMusicFilePath, decompressedMusicFilePath);
+        HuffmanTree.Decompress(compressedRandomFilePath, decompressedRandomFilePath);
+
+        // Assert that the decompressed files are the same as the original files
+        Assert.True(AreFilesEqual(musicFilePath, decompressedMusicFilePath));
+        Assert.True(AreFilesEqual(randomFilePath, decompressedRandomFilePath));
+    }
+
+    public static class LoremIpsumGenerator
+    {
+        private static readonly Random _random = new Random();
+
+        private static readonly string[] _words = new string[] {
+        "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur",
+        "adipiscing", "elit", "sed", "do", "eiusmod", "tempor",
+        "incididunt", "ut", "labore", "et", "dolore", "magna",
+        "aliqua", "Ut", "enim", "ad", "minim", "veniam",
+        "quis", "nostrud", "exercitation", "ullamco", "laboris",
+        "nisi", "ut", "aliquip", "ex", "ea", "commodo",
+        "consequat", "Duis", "aute", "irure", "dolor", "in",
+        "reprehenderit", "in", "voluptate", "velit", "esse",
+        "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur",
+        "Excepteur", "sint", "occaecat", "cupidatat", "non",
+        "proident", "sunt", "in", "culpa", "qui", "officia",
+        "deserunt", "mollit", "anim", "id", "est", "laborum"
+    };
+
+        public static string GenerateLoremIpsum(int length)
+        {
+            StringBuilder sb = new StringBuilder(length);
+
+            while (sb.Length < length)
+            {
+                sb.Append(_words[_random.Next(_words.Length)]);
+                sb.Append(" ");
+            }
+
+            return sb.ToString().Substring(0, length);
+        }
+    }
+
+
+    private bool AreFilesEqual(string filePath1, string filePath2)
+    {
+        byte[] file1Bytes = File.ReadAllBytes(filePath1);
+        byte[] file2Bytes = File.ReadAllBytes(filePath2);
+
+        if (file1Bytes.Length != file2Bytes.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < file1Bytes.Length; i++)
+        {
+            if (file1Bytes[i] != file2Bytes[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ ce100-hw3-bedirhankurt-ahmetkaya/ce100-hw3-algo-lib.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HuffmanTree
+{
+    private readonly HuffmanNode _rootNode;
+    private readonly Dictionary<char, string> _encodingDictionary;
+
+    public HuffmanTree(byte[] data)
+    {
+        // Calculate the frequency of each byte in the data
+        Dictionary<byte, int> frequencyTable = new Dictionary<byte, int>();
+        foreach (byte b in data)
+        {
+            if (!frequencyTable.ContainsKey(b))
+            {
+                frequencyTable[b] = 0;
+            }
+            frequencyTable[b]++;
+        }
+
+        // Build a Huffman tree from the frequency table
+        List<HuffmanNode> nodes = frequencyTable
+            .Select(pair => new HuffmanNode(pair.Key, pair.Value))
+            .ToList();
+        while (nodes.Count > 1)
+        {
+            nodes.Sort((a, b) => a.Frequency - b.Frequency);
+            HuffmanNode newNode = new HuffmanNode(nodes[0], nodes[1]);
+            nodes.RemoveRange(0, 2);
+            nodes.Add(newNode);
+        }
+
+        _rootNode = nodes[0];
+
+        // Build an encoding dictionary from the Huffman tree
+        _encodingDictionary = new Dictionary<char, string>();
+        BuildEncodingDictionary(_rootNode, "");
+    }
+
+    public Dictionary<char, string> EncodingDictionary => _encodingDictionary;
+
+    public byte[] Compress(byte[] data)
+    {
+        // Encode the data using the Huffman tree
+        List<bool> encodedBits = new List<bool>();
+        foreach (byte b in data)
+        {
+            string encodedValue = _encodingDictionary[(char)b];
+            foreach (char c in encodedValue)
+            {
+                encodedBits.Add(c == '1');
+            }
+        }
+
+        // Convert the encoded bits to bytes
+        byte[] compressedData = new byte[(encodedBits.Count + 7) / 8];
+        for (int i = 0; i < encodedBits.Count; i++)
+        {
+            if (encodedBits[i])
+            {
+                compressedData[i / 8] |= (byte)(1 << (7 - (i % 8)));
+            }
+        }
+
+        return compressedData;
+    }
+
+    public byte[] Decompress(byte[] data)
+    {
+        // Convert the compressed data to encoded bits
+        List<bool> encodedBits = new List<bool>();
+        for (int i = 0; i < data.Length; i++)
+        {
+            byte b = data[i];
+            for (int j = 0; j < 8; j++)
+            {
+                encodedBits.Add((b & (1 << (7 - j))) != 0);
+            }
+        }
+
+        // Decode the encoded bits using the Huffman tree
+        List<byte> decodedBytes = new List<byte>();
+        HuffmanNode currentNode = _rootNode;
+        foreach (bool bit in encodedBits)
+        {
+            if (bit)
+            {
+                currentNode = currentNode.RightChild;
+            }
+            else
+            {
+                currentNode = currentNode.LeftChild;
+            }
+
+            if (currentNode.Value.HasValue)
+            {
+                decodedBytes.Add(currentNode.Value.Value);
+                currentNode = _rootNode;
+            }
+        }
+
+        return decodedBytes.ToArray();
+    }
+
+    private void BuildEncodingDictionary(HuffmanNode node, string prefix)
+    {
+        if (node.Value.HasValue)
+        {
+            _encodingDictionary[node.Value.Value] = prefix;
+        }
+        else
+        {
+            BuildEncodingDictionary(node.LeftChild, prefix + "0");
+            BuildEncodingDictionary(node.RightChild, prefix + "1");
+        }
+    }
+
+    private class HuffmanNode
+    {
+        public HuffmanNode(byte value)
+        {
+            Value = value;
+            Frequency = 1;
+        }
+        public HuffmanNode(HuffmanNode leftChild, HuffmanNode rightChild)
+        {
+            LeftChild = leftChild;
+            RightChild = rightChild;
+            Frequency = leftChild.Frequency + rightChild.Frequency;
+        }
+
+        public byte? Value { get; }
+
+        public int Frequency { get; }
+
+        public HuffmanNode LeftChild { get; }
+
+        public HuffmanNode RightChild { get; }
+    }
+}
